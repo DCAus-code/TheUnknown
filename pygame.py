ebb4c08b19a27e5e369cd73edfba526af2ee3455
dca <outlaw.polity0g@icloud.com>
2022-11-27 23:41:09 -0500

    Create pygame.py

---
+++ pygame.py
@@ -0,0 +1,7 @@
+import turtle
+
+window = turtle.Screen()
+window.update() # to show the image
+turtle.write("The Game is under construction.(still) This version is not avalible. Please wait for a constructed version. Thanks")
+input("Press enter to exit ;)")
+mainloop()
